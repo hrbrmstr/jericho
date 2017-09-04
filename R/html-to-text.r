16e1cdf6d2c2f5ca2c9389a63dd4bcf3683d17ab
boB Rudis <bob@rud.is>
2017-09-04 13:04:46 -0400

    docs & basic tests

--- R/html-to-text.r
+++ R/html-to-text.r
@@ -4,6 +4,13 @@
 #' @param include_attributes Sets whether any attribute values are included in the output
 #' @return character vector of cleaned text
 #' @export
+#' @examples
+#' fils <- list.files(system.file("extdata", package="jericho"), full.names=TRUE)
+#' for(f in fils) {
+#'   d <- readBin(f, "raw", file.size(f))
+#'   d <- rawToChar(d)
+#'   html_to_text(d)
+#' }
 html_to_text <- function(x, include_attributes = FALSE) {
   x <- x[1]
   h2t <- J("is.rud.jericho.App")$html_to_text
@@ -19,6 +26,12 @@ html_to_text <- function(x, include_attributes = FALSE) {
 #' @param x HTML content (length 1 character vector)
 #' @return character vector of cleaned text
 #' @export
+#' @examples
+#' for(f in fils) {
+#'   d <- readBin(f, "raw", file.size(f))
+#'   d <- rawToChar(d)
+#'   render_html_to_text(d)
+#' }
 render_html_to_text <- function(x) {
   x <- x[1]
   r2t <- J("is.rud.jericho.App")$render_html_to_text
--- tests/testthat/test-jericho.R
+++ tests/testthat/test-jericho.R
@@ -1,6 +1,27 @@
 context("basic functionality")
 test_that("we can do something", {
 
-  #expect_that(some_function(), is_a("data.frame"))
+  fils <- list.files(system.file("extdata", package="jericho"), full.names=TRUE)
+
+  lapply(fils, function(f) {
+
+    d <- readBin(f, "raw", file.size(f))
+    d <- rawToChar(d)
+
+    list(
+      h = html_to_text(d),
+      b = render_html_to_text(d)
+    )
+
+  }) -> x
+
+  expect_equal(x[[1]]$h, "Jericho HTML Parser Test Document Test HTML Document")
+  expect_equal(x[[1]]$b, "Test HTML Document")
+
+  expect_equal(x[[2]]$h, "SegmentTest SegmentTest p1 p2 p3 p4 p5 p6 p7 find text 2 find text 3 bla find text 4 find text 5 <#unregistered tag type#>")
+  expect_equal(x[[2]]$b, "SegmentTest\r\n\r\n p1\r\n\r\n p2\r\n\r\np3\r\n\r\np4\r\n\r\np5\r\n\r\np6\r\n\r\np7\r\n\r\nfind text 2\r\nfind text 3\r\nbla\r\nfind text 4\r\nfind text 5 <#unregistered tag type#>")
+
+  expect_equal(x[[5]]$h, "Jericho HTML Parser Test Document Test HTML Document")
+  expect_equal(x[[5]]$b, "Test HTML Document")
 
 })
